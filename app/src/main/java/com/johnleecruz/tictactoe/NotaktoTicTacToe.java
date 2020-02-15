23d836f1dee58eab548ce1b20530f78c37b555d3
John Lee-Cruz <jdleecruz@gmail.com>
2020-02-15 17:15:24 -0600

    Created new class NotaktoTicTacToe.

---
+++ app/src/main/java/com/johnleecruz/tictactoe/NotaktoTicTacToe.java
@@ -0,0 +1,220 @@
+package com.johnleecruz.tictactoe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+public class NotaktoTicTacToe extends Activity implements View.OnClickListener {
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean player1Turn = true;
+
+    private int roundCount;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+    private TextView outputText;
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.numerical_tictactoe);
+
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+        outputText = findViewById(R.id.output_textview);
+
+        editText = findViewById(R.id.edit_text_players);
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetGame();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+
+        String letter = editText.getText().toString();
+
+        if (player1Turn) {
+            if (letter.equals("X") || letter.equals("O")) {
+                ((Button) v).setText(letter);
+                outputText.setText("");
+
+                if (!checkForWin() && roundCount != 8) {
+                    Toast.makeText(this, "Player 2's Turn.", Toast.LENGTH_SHORT).show();
+                }
+                player1Turn = false;
+
+                if(checkForWin()) {
+                    player1Turn = true;
+                    if(player1Turn) {
+                        player1Wins();
+                    }
+                }
+
+            } else {
+                outputText.setText("Invalid input. Must enter a 'X' or an 'O'.");
+                player1Turn = true;
+            }
+        }
+        else {
+            if (letter.equals("X") || letter.equals("O")) {
+                ((Button) v).setText(letter);
+                outputText.setText("");
+
+                if (!checkForWin() && roundCount != 8) {
+                    Toast.makeText(this, "Player 1's Turn.", Toast.LENGTH_SHORT).show();
+                }
+
+                if(checkForWin()) {
+                    if(!player1Turn) {
+                        player2Wins();
+                    }
+                }
+
+                player1Turn = true;
+            } else {
+                outputText.setText("Invalid input. Must enter a 'X' or an 'O'.");
+                player1Turn = false;
+            }
+        }
+
+        roundCount++;
+
+        if(roundCount == 9) {
+            draw();
+        }
+
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if(field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if(field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if(field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if(field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 Wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 Wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void updatePointsText() {
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard() {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    private void resetGame() {
+        player2Points = 0;
+        player1Points = 0;
+        editText.setText("");
+        outputText.setText("");
+        updatePointsText();
+        resetBoard();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+
+    }
+}
+
+
